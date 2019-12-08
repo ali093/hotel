9c86b8ad73a903253289bdc7195e877b96add47d
ali093 <mister.ali.mamedov@gmail.com>
2019-12-09 01:07:13 +0400

    first commint

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 $(function(){
   alert("vse qaqa qoshulduq");
+/*
+ * qaqa bele asandi ele?
+
+bu ikiye blunen yer nedi
+ biri sen biri menmi 
+  q
+  yox qaqa gosterir ki ne deyishibsen evvel nece idi indi necedi
+  burdami indi 
+  
+*/ 
 
 });
